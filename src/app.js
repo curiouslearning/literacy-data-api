15959edf7391c25518cb278f23de01ecaec01403
Eric GT <bookjunkie93@gmail.com>
2021-05-17 16:01:10 -0400

    set up dependencies and initial project hierarchy

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const url = require('url');
+const express = require('express');
+const { Client } = require('@google-cloud/bigquery');
+const app = express();
+
+app.get('fetch_latest*', async (req, res) => {
+  
+});
